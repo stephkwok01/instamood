db3e30913eda4c3f32e699feb2935c379d590e8f
Allie Ivener <adi2hf@virginia.edu>
2016-06-09 13:26:25 +0200

    skeleton code for instamood

---
+++ skeleton/script.js
@@ -0,0 +1,26 @@
+var API_DOMAIN = "https://api.instagram.com/v1";
+var RECENT_MEDIA_PATH = "/users/self/media/recent";
+// what do you think a variable in all caps means?
+
+$(document).ready(function() {
+  var token = window.location.hash;
+  if (!token) {
+    window.location.replace("./login.html");
+  }
+  token = token.replace("#", "?"); // Prepare for query parameter
+  var mediaUrl = API_DOMAIN + RECENT_MEDIA_PATH + token;
+
+  $.ajax({
+    url: mediaUrl,
+    dataType: "jsonp",
+    success: handleResponse,
+    error: function() {
+      alert("there has been an error...")
+    }
+  })
+});
+
+function handleResponse(response) {
+  console.log(response);
+  // add stuff here!
+}
